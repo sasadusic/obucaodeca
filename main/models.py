034e3304eb87b591cf840a5a39c2a3fe7ea77f59
sasadusic <www.sasadusic0110989@gmail.com>
2024-04-04 16:04:04 +0200

    za danas tosta

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 import os
@@ -77,3 +78,4 @@ class Odeca(models.Model):
 
     def __str__(self):
         return self.naziv
+    
